61a9f843128b7fc716c02596faa852cf8d74759f
jacob <jacobsolomongolub@gmail.com>
2024-09-16 23:01:32 +0100

    Made foundation for Player and abstract Game classes

---
+++ game.cpp
@@ -0,0 +1,40 @@
+#include "game.h"
+#include <string>
+#include <initializer_list>
+#include <tuple>
+
+Player::Player(std::string myName, int startingPoints){
+    name = myName;
+    points = startingPoints;
+}
+
+void Player::addPoints(int number){
+    points+=points;
+}
+
+void Player::deductPoints(int number){
+    points-=number;
+}
+
+void Player::enterGame(){
+    inGame = true;
+}
+
+void Player::leaveGame(){
+    inGame = false;
+}
+
+void Player::showHand() const {
+    hand.show();
+}
+
+Game::Game(std::vector<Player*> myPlayers){
+    for(const auto player: myPlayers){;
+        this->addPlayer(player);
+    }
+}
+
+void Game::addPlayer(Player* myPlayer){
+    players.push_back(myPlayer);
+    numberOfPlayers++;
+}
---
+++ game.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <vector>
+#include <string>
+#include "cards.h"
+
+
+class Player{
+public:
+    std::string name;
+    Cards::Hand hand;
+
+    Player(std::string,int startingPoints=0);
+    void addPoints(int);
+    void deductPoints(int);
+    void leaveGame();
+    void enterGame();
+    void showHand() const;
+
+private:
+    int points = 0;
+    bool inGame = false;
+
+};
+
+
+class Game{
+public:
+    Cards::Deck deck;
+    std::vector<Player*> players;
+
+    Game(std::vector<Player*>);
+    virtual void play() = 0;
+
+private:
+    int numberOfPlayers = 0;
+
+    void addPlayer(Player*);
+    virtual bool endCondition() = 0;
+};
